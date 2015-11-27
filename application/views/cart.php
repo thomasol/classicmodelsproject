f15628229733b4d661156327d4d7d9f9c1472f95
thomasol <thomas.o.loughlin@hotmail.com>
2015-11-27 12:54:49 +0000

    small changes made during class

--- application/views/cart.php
+++ application/views/cart.php
@@ -6,22 +6,22 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <title>Cart | E-Shopper</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/font-awesome.min.css" rel="stylesheet">
-    <link href="css/prettyPhoto.css" rel="stylesheet">
-    <link href="css/price-range.css" rel="stylesheet">
-    <link href="css/animate.css" rel="stylesheet">
-	<link href="css/main.css" rel="stylesheet">
-	<link href="css/responsive.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.min.css"); ?>" />
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/font-awesome.min.css"); ?>" />
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/prettyPhoto.css"); ?>" />
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/price-range.css"); ?>" />
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/animate.css"); ?>" />
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/main.css"); ?>" />
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/responsive.css"); ?>" />
     <!--[if lt IE 9]>
     <script src="js/html5shiv.js"></script>
     <script src="js/respond.min.js"></script>
     <![endif]-->       
-    <link rel="shortcut icon" href="images/ico/favicon.ico">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
+    <link rel="shortcut icon" href="<?php echo base_url("assets/images/ico/favicon.ico"); ?>" />
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo base_url("assets/images/ico/apple-touch-icon-144-precomposed.png"); ?>" />
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo base_url("images/ico/apple-touch-icon-114-precomposed.png"); ?>" />
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo base_url("images/ico/apple-touch-icon-72-precomposed.png"); ?>" />
+    <link rel="apple-touch-icon-precomposed" href="<?php echo base_url("images/ico/apple-touch-icon-57-precomposed.png"); ?>" />
 </head><!--/head-->
 
 <body>
--- application/views/index.php
+++ application/views/index.php
@@ -58,27 +58,27 @@
 				<div class="row">
 					<div class="col-sm-4">
 						<div class="logo pull-left">
-							<a href="index.php"><img src="<?php echo base_url("assets/images/home/logo.png"); ?>" alt="" /></a>
+							<a href="index.php"><img src="<?php echo base_url("assets/images/logo.jpg"); ?>" alt="" height="39" width="139" /></a>
 						</div>
 						<div class="btn-group pull-right">
 							<div class="btn-group">
 								<button type="button" class="btn btn-default dropdown-toggle usa" data-toggle="dropdown">
-									USA
+									IRELAND
 									<span class="caret"></span>
 								</button>
 								<ul class="dropdown-menu">
-									<li><a href="#">Canada</a></li>
+									<li><a href="#">USA</a></li>
 									<li><a href="#">UK</a></li>
 								</ul>
 							</div>
 							
 							<div class="btn-group">
 								<button type="button" class="btn btn-default dropdown-toggle usa" data-toggle="dropdown">
-									DOLLAR
+									EURO
 									<span class="caret"></span>
 								</button>
 								<ul class="dropdown-menu">
-									<li><a href="#">Canadian Dollar</a></li>
+									<li><a href="#">Dollar</a></li>
 									<li><a href="#">Pound</a></li>
 								</ul>
 							</div>
@@ -87,8 +87,8 @@
 					<div class="col-sm-8">
 						<div class="shop-menu pull-right">
 							<ul class="nav navbar-nav">
-								<li><a href="#"><i class="fa fa-user"></i> Account</a></li>
-								<li><a href="#"><i class="fa fa-star"></i> Wishlist</a></li>
+								<li><a href="<?php echo site_url('ProductsController/account'); ?>"><i class="fa fa-user"></i> Account</a></li>
+								<li><a href="<?php echo site_url('ProductsController/checkout'); ?>"><i class="fa fa-star"></i> Wishlist</a></li>
 								<li><a href="<?php echo site_url('ProductsController/checkout'); ?>"><i class="fa fa-crosshairs"></i> Checkout</a></li>
 								<li><a href="<?php echo site_url('ProductsController/cart'); ?>"></i> Cart</a></li>
 								<li><a href="<?php echo site_url('ProductsController/login'); ?>"><i class="fa fa-lock"></i> Login</a></li>
@@ -158,8 +158,8 @@
 						<div class="carousel-inner">
 							<div class="item active">
 								<div class="col-sm-6">
-									<h1><span>E</span>-SHOPPER</h1>
-									<h2>Free E-Commerce Template</h2>
+									<h1><span>MINI</span>THINGS</h1>
+									<h2>Classic Models Shop</h2>
 									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
 									<button type="button" class="btn btn-default get">Get it now</button>
 								</div>
@@ -170,8 +170,8 @@
 							</div>
 							<div class="item">
 								<div class="col-sm-6">
-									<h1><span>E</span>-SHOPPER</h1>
-									<h2>100% Responsive Design</h2>
+									<h1><span>MINI</span>THINGS</h1>
+									<h2>Classic Models Shop</h2>
 									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
 									<button type="button" class="btn btn-default get">Get it now</button>
 								</div>
@@ -183,8 +183,8 @@
 							
 							<div class="item">
 								<div class="col-sm-6">
-									<h1><span>E</span>-SHOPPER</h1>
-									<h2>Free Ecommerce Template</h2>
+									<h1><span>MINI</span>THINGS</h1>
+									<h2>Classic Models Shop</h2>
 									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
 									<button type="button" class="btn btn-default get">Get it now</button>
 								</div>
@@ -361,7 +361,7 @@
 										<div class="productinfo text-center">
 											<img src="<?php echo base_url("assets/images/products/Berline.jpg"); ?>" alt="" />
 											<h2>$56</h2>
-											<p>Easy Polo Black Edition</p>
+											<p>Green Berline</p>
 											<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
 										</div>
 										<div class="product-overlay">
@@ -386,7 +386,7 @@
 									<div class="productinfo text-center">
 									<img src="<?php echo base_url("assets/images/products/pink-sports-car.jpg"); ?>" alt="" />
 										<h2>$56</h2>
-										<p>Easy Polo Black Edition</p>
+										<p>Pink Sports Car</p>
 										<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
 									</div>
 									<div class="product-overlay">
@@ -411,7 +411,7 @@
 									<div class="productinfo text-center">
 										<img src="<?php echo base_url("assets/images/products/ert_ff164_na.jpg"); ?>" alt="" />
 										<h2>$56</h2>
-										<p>Easy Polo Black Edition</p>
+										<p>Orange Sports Car</p>
 										<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
 									</div>
 									<div class="product-overlay">
