f1a390edbcb3a27536b1ae0dc824c08a9ecf955e
Thomas O' Loughlin <thomas.o.loughlin@hotmail.com>
2015-12-03 01:33:17 +0000

    Registration Working + other small clean up done

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -126,4 +126,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('ProductsModel');
+$autoload['model'] = array('ProductsModel', 'UserModel');
--- application/models/UserModel.php
+++ application/models/UserModel.php
@@ -1,30 +1,51 @@
 <?php
 class UserModel extends CI_Model {
-	 function __construct()
-    {
+	 function __construct() {
         parent::__construct();
     }
 
- function login($email,$password) {
-	$this->db->where("email",$email);
-	$this->db->where("password",$password);
+	 function login($email, $password) {
+		$this->db->where("email", $email);
+		$this->db->where("password", MD5($password));
 
-	$query=$this->db->get("user");
-	if($query->num_rows()>0)
+		$query=$this->db->get("user");
+		if($query->num_rows() > 0) {
+		 foreach($query->result() as $rows) {
+		      $newdata = array(
+		      'user_id'  => $rows->customerNumber,
+		      'user_name'  => $rows->customerName,
+		      'user_email'    => $rows->email,
+		      'logged_in'  => TRUE,
+		    );
+		  }
+		   $this->session->set_userdata($newdata);
+		   return true;
+	    }
+		  return false;
+	}
+
+	function registerUser($customerName, $firstName, $lastName, $email, $password, $phone, $address1, $address2, $city, $state, $postalCode, $country) {
+	    $data = array(
+			'customerName' => $customerName,
+			'contactFirstName' => $firstName,
+			'contactLastName' => $lastName,
+			'email' => $email,
+			'password' => $this->hashPassword($password),
+			'phone' => $phone,
+			'addressLine1' => $address1,
+			'addressLine2' => $address2,
+			'city' => $city,
+			'state' => $state,
+			'country' => $country,
+			'postalCode' => $postalCode,
+			//'created_at' => date('Y-m-j H:i:s'),
+		);
+		return $this->db->insert('customers', $data);
+	}
+
+	function hashPassword($password)
 	{
-	 foreach($query->result() as $rows)
-	  {
-	      $newdata = array(
-	      'user_id'  => $rows->id,
-	      'user_name'  => $rows->username,
-	      'user_email'    => $rows->email,
-	      'logged_in'  => TRUE,
-	    );
-	   }
-	   $this->session->set_userdata($newdata);
-	   return true;
-	  }
-	  return false;
+		return md5($password);
 	}
 }
 ?>
